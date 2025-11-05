e254bc24c54c66cb6f47c253526c009689c18081
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2025-11-04 21:20:19 -0300

    Finalizado

--- ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
+++ ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
@@ -1,32 +1,52 @@
 package org.example.service;
 
 import org.example.model.Produto;
+import org.example.repository.ProdutoRepository;
+import org.example.repository.ProdutoRepositoryImpl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class ProdutoServiceImpl implements ProdutoService{
+
+    ProdutoRepository repository = new ProdutoRepositoryImpl();
+
     @Override
-    public Produto cadastrarProduto(Produto produto) {
+    public Produto cadastrarProduto(Produto produto)throws SQLException {
+        if(produto.getPreco()<0){
+            throw new IllegalArgumentException("Preço deve ser positivo.");
+        }
+
+        return repository.save(produto);
+    }
+
+    @Override
+    public List<Produto> listarProdutos()throws SQLException {
+        return repository.findAll();
+    }
+
+    @Override
+    public Produto buscarPorId(int id)throws SQLException {
         return null;
     }
 
     @Override
-    public List<Produto> listarProdutos() {
+    public Produto atualizarProduto(Produto produto, int id)throws SQLException {
+        Produto prod = repository.findById(id);
+        produto.setId(id);
+
+        repository.update(produto);
         return null;
     }
 
     @Override
-    public Produto buscarPorId(int id) {
-        return null;
-    }
+    public boolean excluirProduto(int id)throws SQLException {
+        Produto prod = repository.findById(id);
+        if(prod == null){
+            return false;
+        }
 
-    @Override
-    public Produto atualizarProduto(Produto produto, int id) {
-        return null;
-    }
-
-    @Override
-    public boolean excluirProduto(int id) {
-        return false;
+        repository.deleteById(id);
+        return true;
     }
 }
--- ExercicioJava/src/main/java/org/example/util/ConexaoBanco.java
+++ ExercicioJava/src/main/java/org/example/util/ConexaoBanco.java
@@ -1,10 +1,16 @@
 package org.example.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoBanco {
 
-    public static Connection conectar(){
-        return null;
+    private static final String url = "jdbc:mysql://localhost:3306/TestesAtv";
+    private static final String user= "root";
+    private static final String pass= "mysqlPW";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(url,user,pass);
     }
 }
